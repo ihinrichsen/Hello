b8a6b456e8748a95c5dabdc9a89a320ee2443523
ihinrichsen <46791758+ihinrichsen@users.noreply.github.com>
2019-01-17 12:57:35 -0600

    Initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Izzy Hinrichsen
+ * Date: 2019/01/17
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Izzy Hinrichsen\n");
+
+  return 0;
+}
